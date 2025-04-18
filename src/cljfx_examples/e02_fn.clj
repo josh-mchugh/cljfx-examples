845303c2b287a01842190584ba342bb5f085c731
Josh McHugh <josh.mchugh@mailbox.org>
2025-04-17 23:40:21 -0400

    Second example from Cljfx with functions building text input with labels

---
+++ src/cljfx_examples/e02_fn.clj
@@ -0,0 +1,19 @@
+(ns e02-fn
+  (:require [cljfx.api :as fx]))
+
+(defn text-input
+  [{:keys [label]}]
+  {:fx/type :v-box
+   :children [{:fx/type :label :text label}
+              {:fx/type :text-field}]})
+
+(fx/on-fx-thread
+  (fx/create-component
+   {:fx/type :stage
+    :showing true
+    :scene {:fx/type :scene
+            :root {:fx/type :v-box
+                   :children [{:fx/type text-input
+                               :label "First Name"}
+                              {:fx/type text-input
+                               :label "Last Name"}]}}}))
