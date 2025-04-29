86f303bbae759f284037c7e033c88ef5d82d1530
Josh McHugh <josh.mchugh@mailbox.org>
2025-04-28 23:52:06 -0400

    Example 3 - mapping event handler with state

---
+++ src/cljfx_examples/e03_map_event-handler.clj
@@ -0,0 +1,27 @@
+(ns cljfx-examples.e03-map-event-handler
+  (:require [cljfx.api :as fx])
+  (:import [javafx.scene.input KeyCode KeyEvent]))
+
+(def *state
+  (atom {:fx/type :stage
+         :always-on-top true
+         :style :transparent
+         :showing true
+         :scene {:fx/type :scene
+                 :fill :transparent
+                 :on-key-pressed {:event/type :event/scene-key-press}
+                 :stylesheets #{"styles.css"}
+                 :root {:fx/type :v-box
+                        :children [{:fx/type :label :text "Hi! What's your name?"}
+                                   {:fx/type :text-field}]}}}))
+
+(defn map-event-handler [e]
+  (when (and (= :event/scene-key-press (:event/type e))
+             (= KeyCode/ESCAPE (.getCode ^KeyEvent (:fx/event e))))
+    (reset! *state nil)))
+
+(def render
+  (fx/create-renderer
+   :opts {:fx.opt/map-event-handler map-event-handler}))
+
+(fx/mount-renderer *state render)
