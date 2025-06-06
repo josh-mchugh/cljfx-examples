e8ee887a66881c2531bc9d21bb12542fc2aeb90e
Josh McHugh <josh.mchugh@mailbox.org>
2025-06-05 23:16:44 -0400

    Example number 5

---
+++ src/cljfx_examples/e05_fn_fx_like_state.clj
@@ -0,0 +1,47 @@
+(ns cljfx-examples.e05-fn-fx-like-state
+  (:require [cljfx.api :as fx]))
+
+(def *state
+  (atom {:first-name "John"
+         :last-name "Doe"}))
+
+(defn text-input [{:keys [label value key]}]
+  {:fx/type :v-box
+   :children [{:fx/type :label
+               :text label}
+              {:fx/type :text-field
+               :on-text-changed {:key key}
+               :text value}]})
+
+(defn root [{:keys [first-name last-name]}]
+  {:fx/type :stage
+   :showing true
+   :scene
+   {:fx/type :scene
+    :root
+    {:fx/type :v-box
+     :children [(if (empty? (str first-name last-name))
+                  {:fx/type :v-box
+                   :effect {:fx/type :drop-shadow}
+                   :children [{:fx/type :label :text "You are very mysterious!"}
+                              {:fx/type :label :text "Please introduce yourself:"}]}
+                  {:fx/type :label
+                   :text (str "You are " first-name " " last-name "!")})
+                {:fx/type text-input
+                 :label "First Name"
+                 :value first-name
+                 :key :first-name}
+                {:fx/type text-input
+                 :label "Last Name"
+                 :value last-name
+                 :key :last-name}]}}})
+
+(defn map-event-handler [event]
+  (swap! *state assoc (:key event) (:fx/event event)))
+
+(def renderer
+  (fx/create-renderer
+   :middleware (fx/wrap-map-desc root)
+   :opts {:fx.opt/map-event-handler map-event-handler}))
+
+(fx/mount-renderer *state renderer)
