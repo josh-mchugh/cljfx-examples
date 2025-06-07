3d30c3072417a8e89f9d6c7238291c778d4034e5
Josh McHugh <josh.mchugh@mailbox.org>
2025-06-06 22:44:03 -0400

    Example 6 with pagination

---
+++ src/cljfx_examples/e06_pagination.clj
@@ -0,0 +1,14 @@
+(ns cljfx-examples.e06-pagination
+  (:require [cljfx.api :as fx]))
+
+(fx/on-fx-thread
+  (fx/create-component
+   {:fx/type :stage
+    :showing true
+    :scene {:fx/type :scene
+            :root {:fx/type :pagination
+                   :page-count 10
+                   :current-page-index 4
+                   :page-factory (fn [i]
+                                   {:fx/type :label
+                                    :text (str "This is a page " (inc i))})}}}))
